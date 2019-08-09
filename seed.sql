02e381ac9e7c44d2c292f194fb3a732b4cf2bf9b
Armond Snowden <armond.z.snowden@gmail.com>
2019-08-09 10:50:42 -0400

    add files

---
+++ seed.sql
@@ -0,0 +1,9 @@
+insert into coffee
+(name, bespoke, size)
+Values
+('Amy','Coffee Black','Grande'),
+('Andy','Frappacino','Venti'),
+('Terry','Espresso','Tall'),
+('Joe','Latte','Venti'),
+('Veronica','Double Espresso','Venti'),
+('Sara','Café au lâit','Grande'),
